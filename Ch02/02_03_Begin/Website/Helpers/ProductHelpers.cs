101bd33d2b81403dc17a22aabd5bc3b03d1e5eb1
Luiz <lgwakano@gmail.com>
2021-04-12 13:09:36 -0600

    _Rating partial view added for DRY purposes + added custom HtmlHelper

---
+++ Ch02/02_03_Begin/Website/Helpers/ProductHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc.Html;
+using HPlusSports.Models;
+
+namespace HPlusSports
+{
+    public static class ProductHelpers
+    {
+        public static IHtmlString Rating(this System.Web.Mvc.HtmlHelper html, ProductRating rating) 
+        {
+            return html.Partial("_Rating", rating);
+        }
+    }
+}
