94afa33ee9f1a9e56c0933c6dd1fa7a45438d43b
Luiz <lgwakano@gmail.com>
2021-04-12 12:23:57 -0600

    UrlHelperExtensions added + refactor Url.Action to new extension

---
+++ Ch02/02_03_Begin/Website/Extensions/UrlHelperExtensions.cs
@@ -0,0 +1,21 @@
+﻿using HPlusSports.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HPlusSports
+{
+    public static class UrlHelperExtensions
+    {
+        public static string Product(this System.Web.Mvc.UrlHelper urlhelper, Product product) 
+        {
+            return Product(urlhelper, product?.SKU);
+        }
+        
+        public static string Product(this System.Web.Mvc.UrlHelper urlhelper, string sku) 
+        {
+            return urlhelper.Action("Product", "Products", new { id = sku });
+        }
+    }
+}
