da39042b3a624fa9373500c6e7513ee4860065af
Luiz <lgwakano@gmail.com>
2022-06-28 00:24:01 -0600

    Added HtmlHelper Extension for Ratings Added PartialView Rating html Refactor duplicated code replaced with PartialView

---
+++ Ch03/03_03_Begin/Website/Helpers/ProductHelpers.cs
@@ -0,0 +1,18 @@
+﻿using HPlusSports.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace HPlusSports
+{
+    public static class ProductHelpers
+    {
+        public static IHtmlString Rating(this System.Web.Mvc.HtmlHelper html, ProductRating rating)
+        {
+            return html.Partial("_Rating", rating);
+        }
+    }
+}
